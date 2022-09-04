b694dc55a4976cf32f63824e94eec1477ef426de
Wes Wilson <wes@iamweswilson.com>
2022-09-04 15:23:58 -0700

    features section

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,17 @@
   <div class="container mx-auto px-12 text-center">
     <Nav />
     <Hero />
+    <Features />
   </div>
 </template>
 
 <script>
   import Nav from '../components/Nav.vue';
   import Hero from '../components/Hero.vue';
+  import Features from '../components/Features.vue';
 
   export default {
     name: "IndexPage",
-    components: { Nav, Hero }
+    components: { Nav, Hero, Features }
   }
 </script>
