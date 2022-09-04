8c21d5ab7042ddf9bfcba124d6d77e520eb46052
Wes Wilson <wes@iamweswilson.com>
2022-09-04 15:40:05 -0700

    diagram section

--- components/Features.vue
+++ components/Features.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="mt-12">
     <h2 class="text-white font-demi text-3xl">Better Storage</h2>
-    <div class="grid gap-8 grid-cols-1 md:grid-cols-2 mt-12 mx-0 lg:mx-12">
+    <div class="grid gap-8 grid-cols-1 md:grid-cols-2 mt-20 mx-0 lg:mx-12">
       <article class="bg-darkblue-700 rounded text-center p-4">
         <div class="bg-darkblue-800 rounded w-full h-full p-8">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F4879E" class="w-10 h-10 mx-auto mb-4">
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <Nav />
     <Hero />
     <Features />
+    <BuiltFor />
   </div>
 </template>
 
@@ -10,9 +11,10 @@
   import Nav from '../components/Nav.vue';
   import Hero from '../components/Hero.vue';
   import Features from '../components/Features.vue';
+  import BuiltFor from '../components/BuiltFor.vue';
 
   export default {
     name: "IndexPage",
-    components: { Nav, Hero, Features }
+    components: { Nav, Hero, Features, BuiltFor }
   }
 </script>
