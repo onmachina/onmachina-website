befc42e26a8965807751ab58be0f980a00e48254
Wes Wilson <wes@iamweswilson.com>
2022-09-04 13:59:13 -0700

    form test

--- components/Hero.vue
+++ components/Hero.vue
@@ -8,19 +8,19 @@
     <p class="text-gray-400 mt-8 font-book">
       We are building <strong class="text-white font-demi">storage first</strong> <em>(resilience, availability, capacity, performance)</em>, <strong class="text-white font-demi">enhanced with blockchain</strong> <em>(decentralized, immutable, verified owner, permissionless)</em> to give control and ownership of data <strong class="text-white font-demi">back to individuals</strong> from the public cloud oligopolists.
     </p>
-    <form name="waitlist" action="/thanks" method="post" netlify netlify-honeypot="bot-field">
+    <form name="waitlist" method="POST" netlify netlify-honeypot="bot-field">
       <p class="text-white mt-12 font-book mb-4">
         Sign up for early access, updates and ways to get involved.
       </p>
       <div class="relative w-full md:w-3/5 mx-auto bg-darkblue-800 p-2 rounded-full">
-        <button type="button" class="absolute right-6 rounded-full p-3 hover:bg-salmon" style="top:17%; right: 14px">
+        <button type="submit" class="absolute right-6 rounded-full p-3 hover:bg-salmon" style="top:17%; right: 14px">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="#363844" class="w-6 h-6 ">
             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
           </svg>
         </button>
         <!-- Hidden input to check for bots -->
-        <input type="hidden" name="waitlist" value="contact" />
-        <input class="w-full py-4 px-4 rounded-full text-sm placeholder-gray-500 bg-gray-300 focus:outline-none focus:ring-salmon focus:border-salmon border-2 border-gray-300" type="email" name="email" placeholder="Add your email address"></input>
+        <input type="hidden" name="bot-field" />
+        <input class="w-full py-4 px-4 rounded-full text-sm placeholder-gray-500 bg-gray-300 focus:outline-none focus:ring-salmon focus:border-salmon border-2 border-gray-300" type="text" name="email" placeholder="Add your email address"></input>
       </div>
     </form>
   </div>
