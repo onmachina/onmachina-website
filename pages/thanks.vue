4ae5b052220b1b433ef1ac821ec547b760a3e030
Wes Wilson <wes@iamweswilson.com>
2022-09-05 17:54:18 -0700

    thanks headline tweak

--- pages/thanks.vue
+++ pages/thanks.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto px-12 text-center">
     <Nav />
     <section class="pt-20 w-full lg:w-3/5 mx-auto">
-      <h1 class="text-5xl md:text-6xl text-white font-demi leading-none"><span class="text-transparent bg-clip-text bg-gradient-to-r from-peach to-sea block">Thank you,</span> your email has been received!</h1>
+      <h1 class="text-5xl md:text-6xl text-white font-demi leading-none"><span class="text-transparent bg-clip-text bg-gradient-to-r from-peach via-salmon to-sea block">Thank you,</span> your email has been received!</h1>
       <p class="text-gray-400 mt-8 mb-32 w-full md:w-3/4 mx-auto font-book">
       You're on the list, we will reach out as soon as possible. In the meantime, feel free to <a href="https://twitter.com/onmachina" class="underline text-white">follow @OnMachina on Twitter</a> for regular updates.
     </p>
