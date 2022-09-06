dcea9c88e92514df763d57945af4cd2ecbc67fc4
Wes Wilson <wes@iamweswilson.com>
2022-09-05 17:45:53 -0700

    revert feature stagger

--- components/Features.vue
+++ components/Features.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="mt-12">
     <h2 class="text-white font-demi text-3xl">Better Storage</h2>
-    <div class="grid gap-8 grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-5 mt-20 mx-0 lg:mx-12">
-      <article class="bg-darkblue-700 rounded text-center p-4 row-start-1 col-start-1 row-span-1 md:row-span-2 feature-card opacity-0">
+    <div class="grid gap-8 grid-cols-1 md:grid-cols-2 grid-rows-2 mt-20 mx-0 lg:mx-12">
+      <article class="bg-darkblue-700 rounded text-center p-4 feature-card opacity-0">
         <div class="bg-darkblue-800 rounded w-full h-full p-8">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F4879E" class="w-10 h-10 mx-auto mb-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
@@ -13,7 +13,7 @@
           <p class="text-white font-medium leading-tight w-3/5 mx-auto"> Nice and easy, developer-friendly API, 100% Open Source Protocol</p>
         </div>
       </article>
-      <article class="bg-darkblue-700 rounded text-center p-4 col-start-1 md:col-start-2 md:row-start-2 md:row-span-2 feature-card opacity-0">
+      <article class="bg-darkblue-700 rounded text-center p-4 feature-card opacity-0">
         <div class="bg-darkblue-800 rounded w-full h-full p-8">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFAA00" class="w-10 h-10 mx-auto mb-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5" />
@@ -22,7 +22,7 @@
           <p class="text-white font-medium leading-tight w-3/5 mx-auto">Predictable pricing model, paid in stable-coins</p>
         </div>
       </article>
-      <article class="bg-darkblue-700 rounded text-center p-4 col-start-1 md:row-start-3 md:row-span-2 feature-card opacity-0">
+      <article class="bg-darkblue-700 rounded text-center p-4 feature-card opacity-0">
         <div class="bg-darkblue-800 rounded w-full h-full p-8">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#5BEBEF" class="w-10 h-10 mx-auto mb-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 01.9-2.7L5.737 5.1a3.375 3.375 0 012.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 01.9 2.7m0 0a3 3 0 01-3 3m0 3h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008zm-3 6h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008z" />
@@ -32,7 +32,7 @@
           <p class="text-white font-medium leading-tight w-3/5 mx-auto">Real infrastructure, no DeFi, BS tokens and scams</p>
         </div>
       </article>
-      <article class="bg-darkblue-700 rounded text-center p-4 md:col-start-2 md:row-start-4 md:row-span-2 feature-card opacity-0">
+      <article class="bg-darkblue-700 rounded text-center p-4 feature-card opacity-0">
         <div class="bg-darkblue-800 rounded w-full h-full p-8">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#EA3347" class="w-10 h-10 mx-auto mb-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
