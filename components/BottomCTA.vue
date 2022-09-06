2bcbf8bcb0ad171a103851895343d66c7d0a2ffd
Wes Wilson <wes@iamweswilson.com>
2022-09-05 17:52:01 -0700

    responsive fix

--- components/BottomCTA.vue
+++ components/BottomCTA.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="p-8 bg-darkblue-800 rounded-md w-full md:w-3/4 mx-auto border-b-4 border-sea">
+  <section class="p-8 bg-darkblue-800 rounded-md w-full md:w-3/4 mx-auto border-b-4 border-sea mt-12 md:mt-0">
     <JoinForm />
     <p class="text-sm italic text-white mt-6">
       We respect your data and will not share it.
